5251a281fb5a7cb037d4f84a9aa31682c586c3b1
Deddy T <deddybuster@hotmail.com>
2016-05-03 18:57:42 +0800

    Qn 2b(i)

---
+++ 2008-zone-b/centuryinseconds.java
@@ -0,0 +1,6 @@
+public class centuryinseconds{
+  public static void main(String[] args){
+    double s = 33600 * 86400.0;
+    System.out.println("In a century, there are " + s + " seconds");
+  }
+}

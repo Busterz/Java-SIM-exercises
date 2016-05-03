63a1a7992421d1e3397c06bb08417127c96a495e
Deddy T <deddybuster@hotmail.com>
2016-05-03 18:56:04 +0800

    Qn 1(b)(ii)

---
+++ 2008-zone-b/printbits.java
@@ -0,0 +1,6 @@
+public class printbits{
+  public static void main(String[] args){
+    double bits = 5*8*Math.pow(1024,3);
+    System.out.println("5 gigabytes is equal to " + bits + " bits");
+  }
+}
